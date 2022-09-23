1b6058da016dcaeb35ef666204ee2650823f3f60
Roman Kuzyk <roman.kuzyk.a@gmail.com>
2022-09-23 21:58:58 +0300

    create default layout

---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container">
+    <Nuxt />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DefaultLayout',
+}
+</script>
+
+<style scoped lang="scss">
+.container {
+  min-height: 100vh;
+}
+</style>

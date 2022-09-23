ffb6c899c9622857eed187b7ae8ebac912250876
Roman Kuzyk <roman.kuzyk.a@gmail.com>
2022-09-23 23:28:11 +0300

    create TheTitle component for reusable page titles

---
+++ components/Common/TheTitle.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1 class="title">
+    <slot></slot>
+  </h1>
+</template>
+
+<script>
+export default {
+  name: 'TheTitle',
+}
+</script>
+
+<style lang="scss" scoped>
+.title {
+  font-size: $font-l;
+  line-height: 42px;
+  font-weight: 700;
+  color: $text-strong;
+}
+</style>

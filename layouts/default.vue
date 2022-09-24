40cf10df9690b373a9c34659816339f64c939153
Roman Kuzyk <roman.kuzyk.a@gmail.com>
2022-09-24 21:41:31 +0300

    add Header

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,12 @@
 <template>
   <div class="container">
+    <header class="header">
+      <Nuxt-link to="/">
+        <img src="~/assets/images/Logo-circle.png" class="header__image" />
+      </Nuxt-link>
+
+      <Nuxt-link class="header__link" to="/login">Log In</Nuxt-link>
+    </header>
     <Nuxt />
   </div>
 </template>
@@ -14,5 +21,25 @@ export default {
 .container {
   padding-top: 75px;
   min-height: 100vh;
+}
+
+.header {
+  position: fixed;
+  top: 0;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  padding: 10px;
+  background-color: $default;
+
+  &__image {
+    width: 150px;
+  }
+
+  &__link {
+    color: $text-main;
+    cursor: pointer;
+  }
 }
 </style>

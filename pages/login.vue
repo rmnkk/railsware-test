5d54ea846338bec7288ddef248b6130cd3dfb536
Roman Kuzyk <roman.kuzyk.a@gmail.com>
2022-09-24 19:54:00 +0300

    create login page

---
+++ pages/login.vue
@@ -0,0 +1,19 @@
+<template>
+  <LoginContent />
+</template>
+
+<script>
+import LoginContent from '~/components/Auth/LoginContent.vue'
+export default {
+  name: 'LoginPage',
+  components: { LoginContent },
+  data() {
+    return {
+      email: '',
+      password: '',
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>

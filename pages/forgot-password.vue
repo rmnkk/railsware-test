eba8d1a61ea242cd5b7af1b3fcc661e834ab66dd
Roman Kuzyk <roman.kuzyk.a@gmail.com>
2022-09-24 21:43:12 +0300

    create forgot password page

---
+++ pages/forgot-password.vue
@@ -0,0 +1,13 @@
+<template>
+  <ForgotPasswordContent />
+</template>
+
+<script>
+import ForgotPasswordContent from '~/components/Auth/ForgotPasswordContent.vue'
+export default {
+  name: 'ForgotPasswordPage',
+  components: { ForgotPasswordContent },
+}
+</script>
+
+<style lang="scss" scoped></style>

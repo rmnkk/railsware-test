722f28e3ce0263ddcecd7e72baedae2f974c26e8
Roman Kuzyk <roman.kuzyk.a@gmail.com>
2022-09-24 21:42:58 +0300

    create ForogotPasswordContent component

---
+++ components/Auth/ForgotPasswordContent.vue
@@ -0,0 +1,24 @@
+<template>
+  <PageContentWrapper>
+    <template #image>
+      <img src="~assets/images/Forgot-password- illustration.svg" />
+    </template>
+
+    <template #title>Forgot Password</template>
+
+    <template #form>
+      <ForgotPasswordForm />
+    </template>
+  </PageContentWrapper>
+</template>
+
+<script>
+import PageContentWrapper from '../Common/PageContentWrapper.vue'
+import ForgotPasswordForm from './ForgotPasswordForm.vue'
+export default {
+  name: 'ForgotPasswordContent',
+  components: { PageContentWrapper, ForgotPasswordForm },
+}
+</script>
+
+<style lang="scss" scoped></style>
